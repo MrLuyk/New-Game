6a86c82d8a488104199314b5d4917b1d50c9fb84
Mr. Luyk <133881094+MrLuyk@users.noreply.github.com>
2025-01-08 17:24:01 +0000

    Skeleton of the Program.cs / wuh / empty GameplayLoop Function.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("wuh");
+        GameplayLoop();
+    }
+    
+    public static void GameplayLoop()
+    {
+        
+    }
+}
